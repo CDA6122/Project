f1f84af2bbd0133a0040e3cd1b4d17800a229d84
David Bruck <dbruck1@fau.edu>
2019-10-23 01:59:04 -0400

    Added some fake simulated events

--- Models/Simulation.cs
+++ Models/Simulation.cs
@@ -13,13 +13,62 @@ namespace Project.Models
 {
     internal static class Simulation
     {
-        internal static IReadOnlyList<SimulationEvent> Run()
+        // TODO: do not assume a constant bitrate
+        internal const decimal Bitrate = 5_000_000; // ~ high quality 1080p video
+
+        // Bandwidth assumptions from whitepaper: http://www.strixsystems.com/products/datasheets/strixwhitepaper_multihop.pdf
+        // 1 hop: 24mbps
+        // 2 hops: 11.9mbps
+        // 3 hops: 8mbps
+        // 4 hops: 6mbps
+
+        internal static (IReadOnlyList<(string name, long size)> files, IReadOnlyList<SimulationEventBase> events) Run()
         {
             // TODO: create actual random events; for now, this will just return some fake events
-            return new ReadOnlyCollection<SimulationEvent>(new[]
-            {
-                new SimulationEvent()
-            });
+            return (files: new ReadOnlyCollection<(string name, long size)>(new[]
+                {
+                    ("ABC", 375_000_000L), // ~10 minutes
+                    ("DEF", 750_000_000L), // ~20 minutes
+                }),
+                events: new ReadOnlyCollection<SimulationEventBase>(new SimulationEventBase[]
+                {
+                    new SimulationEventSimulationStart(),
+
+                    // At 2 minutes in, Node 1 requests first file, nearest copy is on node 4
+                    new SimulationEventFileRequested(TimeSpan.FromSeconds(120d), 1, "Cat videos", 4, 5_000_000M), // ~5mbps out of 8mbps available for 3 hops
+                    new SimulationEventFullyBuffered(TimeSpan.FromSeconds(120d), 1, 0L), // ample bandwidth, no need to buffer
+
+                    // At 4 minutes in, Node 2 requests second file, nearest copy is also on node 4
+                    // Existing connection: nodes 1 to 4, 5mbps out of 8mbps (37.5% available)
+                    // 11.9mbps normally available for 2 hops; 37.5% of that is 4.46mbps; we have to buffer
+                    new SimulationEventFileRequested(TimeSpan.FromSeconds(240d), 2, "TV Show", 4, 4_460_000),
+
+                    // buffering seconds * 4.46mbps = bytes transferred at time of fully-buffered
+                    // (750MB - bytes transferred) / 750MB = %remaining at time of fully-buffered
+                    // 20 minutes to download %remaining of 750MB = 4.46mbps available bandwidth
+                    // 
+                    // solve for %remaining:
+                    // 120second = (6,000,000,000bytes * %remaining) / 4,460,000bps
+                    // %remaining = 89.2%
+                    //
+                    // solve for bytes transfered at time of fully-buffered:
+                    // (6,000,000,000bytes - bytes transferred) / 6,000,000,000bytes = 89.2%
+                    // bytes transferred = 648,000,000
+                    //
+                    // solve for buffering seconds:
+                    // buffering seconds * 4,460,000 = 648,000,000
+                    // buffering seconds = 145.291
+                    new SimulationEventFullyBuffered(TimeSpan.FromSeconds(240d + 145.291d), 2, 648_000_000),
+
+                    // Node 1 finishes first, it took 10minutes to stream 10minutes of video including buffering
+                    new SimulationEventStreamingComplete(TimeSpan.FromSeconds(120d + 600d), 1),
+
+                    // Node 2 finishes second, it took 20minutes plus 145.291seconds of buffering to stream 20minutes of video
+                    new SimulationEventStreamingComplete(TimeSpan.FromSeconds(240d + 1200d + 145.291d), 2),
+
+                    // arbitrary simulation cutoff of new random events after 30 minutes (simulation parameter)
+                    new SimulationEventSimulationEnd(TimeSpan.FromMinutes(30d))
+                }));
         }
     }
 }
--- Models/SimulationEvent.cs
+++ Models/SimulationEvent.cs
@@ -10,7 +10,117 @@ using System.Threading.Tasks;
 
 namespace Project.Models
 {
-    public class SimulationEvent
+    /* Simulation event types (and observables):
+     * SimulationStart (time)
+     * FileRequested(time, requesting node, file, connection node, bandwidth)
+     * FullyBuffered (time, node, size streamed at time of fully buffered)
+     * StreamingComplete (time, node)
+     * SimulationEnd (time)
+     *
+     * ** TODO: support re-routing traffic mid-stream and adjusting bandwidth on complete streams
+     */
+
+    internal abstract class SimulationEventBase
     {
+        internal readonly TimeSpan EventTime;
+        internal protected SimulationEventBase(TimeSpan eventTime)
+        {
+            EventTime = eventTime;
+        }
+    }
+
+    internal sealed class SimulationEventSimulationStart : SimulationEventBase
+    {
+        internal SimulationEventSimulationStart()
+            : base(TimeSpan.Zero)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Simulation started";
+        }
+    }
+
+    internal sealed class SimulationEventFileRequested : SimulationEventBase
+    {
+        internal readonly int RequestingNode;
+        internal readonly string SimulationFile;
+        internal readonly int ConnectionNode;
+        internal readonly decimal Bandwidth;
+        internal SimulationEventFileRequested(TimeSpan eventTime, int requestingNode, string simulationFile, int connectionNode, decimal bandwidth)
+            : base(eventTime)
+        {
+            RequestingNode = requestingNode;
+            SimulationFile = simulationFile;
+            ConnectionNode = connectionNode;
+            Bandwidth = bandwidth;
+        }
+
+        public override string ToString()
+        {
+            return $@"File requested
+                Node {RequestingNode}
+
+                File:
+                {SimulationFile}
+
+                Connection:
+                Node {ConnectionNode}
+                Bandwidth {Bandwidth / 1000000M:#,##0.##}mbps";
+        }
+    }
+
+    internal sealed class SimulationEventFullyBuffered : SimulationEventBase
+    {
+        internal readonly int Node;
+        internal readonly long BytesStreamedAtTimeOfFullyBuffered;
+        internal SimulationEventFullyBuffered(TimeSpan eventTime, int node, long bytesStreamedAtTimeOfFullyBuffered)
+            : base(eventTime)
+        {
+            Node = node;
+            BytesStreamedAtTimeOfFullyBuffered = bytesStreamedAtTimeOfFullyBuffered;
+        }
+
+        public override string ToString()
+        {
+            return $@"File fully buffered
+                Node {Node}
+
+                File size downloaded so far:
+                {BytesStreamedAtTimeOfFullyBuffered / 8000000M:#,##0.##}MB";
+        }
+    }
+
+    internal sealed class SimulationEventStreamingComplete : SimulationEventBase
+    {
+        internal readonly int Node;
+        internal SimulationEventStreamingComplete(TimeSpan eventTime, int node)
+            : base(eventTime)
+        {
+            Node = node;
+        }
+
+        public override string ToString()
+        {
+            return $"File finished streaming on node {Node}";
+        }
+    }
+
+    internal sealed class SimulationEventSimulationEnd : SimulationEventBase
+    {
+        internal SimulationEventSimulationEnd(TimeSpan eventTime)
+            : base(eventTime)
+        {
+        }
+
+        public override string ToString()
+        {
+            return @"Simulation terminated
+
+                Elapsed time: " +
+                (EventTime.TotalHours >= 1 ? $"{EventTime.TotalHours:#,##0} hours and " : "") +
+                $"{EventTime.TotalMinutes % 3600d:#,##0.##} minutes";
+        }
     }
 }
